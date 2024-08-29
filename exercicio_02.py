339e474ccc20a4d0c7ace45fbdb0e1cc6a156f28
Danielle Rainha <daniellegrainha@gmail.com>
2024-08-29 13:41:02 -0300

    Add files via upload

---
+++ exercicio_02.py
@@ -0,0 +1,2 @@
+nome = input("Digite seu nome: ")
+print(f"Olá, é um prazer lhe conhecer, ", nome, "!")
---
+++ exercicio_05.py
@@ -0,0 +1,4 @@
+valor = int(input("Digite um número inteiro: "))
+antecessor = valor - 1
+sucessor = valor + 1
+print(f"O valor digitado foi: {valor}, seu antecessor é {antecessor} e seu sucessor é {sucessor}.")
